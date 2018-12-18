d7ff89e72ae315a0d888306dac328db57890413c
Naveen Pete <naveen.shenoy.blr@gmail.com>
2018-12-18 13:12:19 +0530

    commit demo for built-in pipes and directives

--- apps/day-02/store-app/src/app/products/products.component.ts
+++ apps/day-02/store-app/src/app/products/products.component.ts
@@ -1,22 +1,47 @@
 import { Component } from '@angular/core';
 
+import { Product } from '../models/product';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  productId = 1;
-  productName = 'Dell Inspiron 15 7000';
-  productDescription = 'Whether it’s fast-paced racing or action-packed roleplay, the Dell Inspiron 15 laptop is designed to make your gaming experience truly delightful';
-  productPrice = 45000;
-  productIsAvailable = true;
+  product: Product = new Product();
 
-  onSave() {
-    console.log('Save button clicked');
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'Dell Inspiron 15 7000',
+      description: 'Whether it’s fast-paced racing or action-packed roleplay, the Dell Inspiron 15 laptop is designed to make your gaming experience truly delightful',
+      price: 45000,
+      isAvailable: false
+    },
+    {
+      id: 2,
+      name: 'Dell Vostro',
+      description: 'Vostro description',
+      price: 35000,
+      isAvailable: true
+    },
+    {
+      id: 3,
+      name: 'Dell XPS',
+      description: 'Dell XPS description',
+      price: 95000,
+      isAvailable: true
+    }
+  ];
+
+  currencyCode = 'INR';
+
+  applyBadge(product) {
+    return product.isAvailable ? 'badge-primary' : 'badge-danger';
   }
 
-  onChange(e) {
-    console.log('input event triggered:', e.target.value);
+  onSubmit() {
+    this.products.unshift(this.product);
+    this.product = new Product();
   }
 }
