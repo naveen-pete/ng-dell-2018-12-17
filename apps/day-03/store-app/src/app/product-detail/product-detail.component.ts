8a78ba97faec9d6970cf715053e953ac35c09406
Naveen Pete <naveen.shenoy.blr@gmail.com>
2018-12-19 06:35:14 +0530

    add store app for services demo

---
+++ apps/day-03/store-app/src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  @Input() product: Product = new Product();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  applyBadge() {
+    return this.product.isAvailable ? 'badge-primary' : 'badge-danger';
+  }
+}
